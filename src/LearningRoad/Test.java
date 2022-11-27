57137e69b23b5f8709a51f981ca32b13c4c2a493
kennySweson <kennysweson@gmail.com>
2022-11-27 21:37:23 +0800

    Test java.awt.point

--- src/LearningRoad/Test.java
+++ src/LearningRoad/Test.java
@@ -1,10 +1,15 @@
 package LearningRoad;
 
+import java.awt.Point;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		
+		Point point=new Point();
+		point.x=1;
+		point.y=2;
+		System.out.println(point.toString());
 		
 		
 	}
