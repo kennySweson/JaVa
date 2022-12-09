36e078613f20e684f14b7b6e65f5b10bcc656d92
kennySweson <kennysweson@gmail.com>
2022-12-09 16:02:25 +0800

    Submit 异常处理

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+public class Main{
+	public static void main(String args[]) {
+		int a,b,div=0;
+		Scanner reader=new Scanner(System.in);
+		a=reader.nextInt();
+		b=reader.nextInt();
+		boolean flag=true;
+		
+		try{
+			div=a/b;
+		}catch (Exception e) {
+			// TODO: handle exception
+			flag=false;
+			System.out.println("ArithmeticException!!!");
+		}
 
-public class Main {
-    public static void main(String[] args) {
-        int n;
-        String s;
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        s=DtoH(n);
-        System.out.println(s);
-    }
-    
-
-    public static String DtoH(int n) {
-    	String basic=new String("0123456789ABCDEF");
-    	StringBuilder sb=new StringBuilder();
-    	
-    	while(n>16) {
-    		int temp=n%16;
-    		sb.append(basic.charAt(temp));
-    		n/=16;
-    	}
-    	
-    	sb.append(basic.charAt(n));
-    	sb.reverse();
-    	
-    	return sb.toString();
-    }
-    
+		if(flag) {
+			System.out.print(div);
+		}
+	}
 }
