c2baf48ba7aba7a3105b8c8cc70a97755bf11c31
kennySweson <kennysweson@gmail.com>
2022-12-09 17:29:16 +0800

    submit 自定义异常

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,60 @@
 import java.util.*;
 public class Main{
-	public static void main(String args[]) {
-		int a,b,div=0;
-		Scanner reader=new Scanner(System.in);
-		a=reader.nextInt();
-		b=reader.nextInt();
-		boolean flag=true;
+	
+	public static class AgeException extends Exception{
+		private String message="";
 		
-		try{
-			div=a/b;
-		}catch (Exception e) {
+		public AgeException() {
+			
+		}
+		public AgeException(String s) {
+			// TODO 自动生成的构造函数存根
+			this.message=s;
+		}
+		
+		public String toString() {
+			return message;
+		}
+		
+	}
+	
+	
+	public static boolean checkAge(int age) throws AgeException {
+		AgeException a=new AgeException();
+		if(age<0) {
+			a.message="error";
+			throw a;
+		}
+		else if (age>=10&&age<=79) {
+			return true;
+		}
+		else if((age>=0&&age<=9)||(age>=80&&age<=100)) {
+			return false;
+		}
+		else if (age>100) {
+			a.message="too old";
+			throw a;
+		}
+		return false;
+		
+	}
+	
+	
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int age=sc.nextInt();
+		
+		try {
+			if(checkAge(age)) {
+				System.out.print("OK");
+			}
+			else {
+				System.out.print("fail");
+			}
+		}catch (AgeException a) {
 			// TODO: handle exception
-			flag=false;
-			System.out.println("ArithmeticException!!!");
+			System.out.print(a.toString());
 		}
-
-		if(flag) {
-			System.out.print(div);
-		}
+		
 	}
 }
