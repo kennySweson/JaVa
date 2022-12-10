729b50f6d47001d06670e4c068b1ca45b72dc3b6
kennySweson <kennysweson@gmail.com>
2022-12-11 06:18:06 +0800

    Submit 带finally块的异常处理

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,27 @@
 import java.util.*;
 public class Main{
 	
-	public static class AgeException extends Exception{
-		private String message="";
-		
-		public AgeException() {
-			
-		}
-		public AgeException(String s) {
-			// TODO 自动生成的构造函数存根
-			this.message=s;
-		}
-		
-		public String toString() {
-			return message;
-		}
-		
-	}
-	
-	
-	public static boolean checkAge(int age) throws AgeException {
-		AgeException a=new AgeException();
-		if(age<0) {
-			a.message="error";
-			throw a;
-		}
-		else if (age>=10&&age<=79) {
-			return true;
-		}
-		else if((age>=0&&age<=9)||(age>=80&&age<=100)) {
-			return false;
-		}
-		else if (age>100) {
-			a.message="too old";
-			throw a;
-		}
-		return false;
-		
-	}
-	
-	
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
-		int age=sc.nextInt();
+		String str=sc.nextLine();
+		String[] ans=str.split(",");
 		
-		try {
-			if(checkAge(age)) {
-				System.out.print("OK");
+		int a = 0;
+		
+		for(int i=0;i<ans.length;i++) {
+			boolean flag=true;
+			try {
+				a=Integer.valueOf(ans[i]);
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("NumberFormatException");
+				flag=false;
+			} finally {
+				if(flag) {
+					System.out.println(a);
+				}
+				System.out.println("the index is "+i);
 			}
-			else {
-				System.out.print("fail");
-			}
-		}catch (AgeException a) {
-			// TODO: handle exception
-			System.out.print(a.toString());
 		}
 		
 	}
