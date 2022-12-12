6bd0d0f2e60ad6fd97b7a20cb0e4aec4981a47e2
kennySweson <kennysweson@gmail.com>
2022-12-12 17:20:06 +0800

    Submit transform first letter of words

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,30 @@ public class Main{
 	
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		String[] ans=str.split(",");
-		
-		int a = 0;
-		
-		for(int i=0;i<ans.length;i++) {
-			boolean flag=true;
-			try {
-				a=Integer.valueOf(ans[i]);
-			} catch (Exception e) {
-				// TODO: handle exception
-				System.out.println("NumberFormatException");
-				flag=false;
-			} finally {
-				if(flag) {
-					System.out.println(a);
-				}
-				System.out.println("the index is "+i);
-			}
+		String sentence = sc.nextLine();
+
+		// 将整个句子转换为小写
+		sentence = sentence.toLowerCase();
+
+		// 将每个单词的首字母转换为大写
+		String[] words = sentence.split(" ");
+		for (int i = 0; i < words.length; i++) {
+		  String word = words[i];
+		  String firstLetter = word.substring(0, 1);
+		  firstLetter = firstLetter.toUpperCase();
+		  word = firstLetter + word.substring(1);
+		  words[i] = word;
 		}
+
+		// 将改变后的单词重新组合成句子
+		sentence = String.join(" ", words);
+
+		// 输出改变后的句子
+		System.out.println(sentence); // 输出: Hello World
+
+		
+		
+		
 		
 	}
 }
