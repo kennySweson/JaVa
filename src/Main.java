b6b015e4af997feb6784b64814dde5ca46a37259
kennySweson <kennysweson@gmail.com>
2022-12-21 16:04:30 +0800

    Submit 正方形类

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,37 @@
 import java.util.*;
 public class Main{
 	public static void main(String args[]) {
-		double w,h;
-		Rectangle r;
-		Square s;
-		Scanner in=new Scanner(System.in);
-		w=in.nextDouble();
-		h=in.nextDouble();
-		//根据运行结果，创建合适的对象；
-		r=new Rectangle(w, h);
-		System.out.printf("%.2f\n",r.getArea());  //15.00
-		//创建合适的对象；
-		s=new Square(w);
-		System.out.printf("%.2f\n",s.getArea());  //25.00
-		System.out.printf("%.2f\n",s.getLength());  //5.00
-		//创建合适的对象；
-		r=new Rectangle(h, h);
-		System.out.printf("%.2f\n",r.getArea());  //9.00
-	}
-}
-class Rectangle{
-	private double width,height;
-	Rectangle(double width,double height){
-		this.width=width;
-		this.height=height;
-	}
-	double getArea(){
-		return width*height;
-	}
-	double getWidth(){
-		return width;
-	}
-	double getHeight(){
-		return height;
-	}
+		double a,area;
+		Square sq;
+		Scanner reader=new Scanner(System.in);
+		a=reader.nextDouble();
+		/*以缺省方式创建Square对象*/
+		sq=new Square();
+		/*调用Square对象的getArea方法，返回值存放在area中*/
+		area=sq.getArea();
+		System.out.printf("%.1f\n",area);
+		/*以带参数a的方式创建Square对象*/
+		sq=new Square(a);
+		System.out.printf("%.1f\n",sq.getArea());
+	}    
 }
 
-class Square extends Rectangle{
-	private static double length;
+class Square{
+	double length;
+	/*带一个参数的构造方法*/
 	public Square(double l) {
-		super(l, l);
-		this.length=l;
+		length=l;
 		// TODO 自动生成的构造函数存根
 	}
-	
-	public double getLength() {
-		return length;
+
+	/*缺省构造方法，即不带参数的构造方法，将length值设置为1.0*/
+	public Square() {
+		length=1;
 	}
-	
+
+
+	/*公共方法getArea，返回正方形面积*/
+	double getArea() {
+		return length*length;
+	}
 }
